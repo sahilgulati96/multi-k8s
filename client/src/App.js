4bb27b2df56b9d8564c8c21d1b0abd0f90325ca5
Sahil Gulati <sahilgulati102@gmail.com>
2022-05-27 16:37:26 +0530

    dev

--- client/src/App.js
+++ client/src/App.js
@@ -19,8 +19,8 @@ function App() {
           >
             Learn React 3
           </a>
-          <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
+          <Link to="/">React Home</Link>
+          <Link to="/otherpage">Hi Page</Link>
         </header>
         <div>
           <Route exact path="/" component={Fib} />
